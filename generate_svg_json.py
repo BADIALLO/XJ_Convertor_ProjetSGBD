823cc09095d1df31248bd33337f19e0bbf9ff714
baramata <46568746+baramata@users.noreply.github.com>
2019-01-17 07:20:22 +0000

    Update generate_svg_json.py

--- generate_svg_json.py
+++ generate_svg_json.py
@@ -1,94 +1,95 @@
 import pip,sys,os
 import svgwrite
-import extractionJSON
+import validation
+import extractionJSON,XJ_Convertor
 import requests
 
-myJsonData = extractionJSON.parseJSON(sys.argv[1])
-listEntites = []
-# Recuperation de la liste des entités
+if XJ_Convertor.fflag == 1 and XJ_Convertor.json == 1:
+    myJsonData = extractionJSON.parseJSON(XJ_Convertor.fichierInput)
+elif XJ_Convertor.hflag == 1 and XJ_Convertor.json == 1:
+    response = requests.get(url = XJ_Convertor.url)
+    myJsonData = response.json() 
 
-for i in range(len(myJsonData)):
-    keys = myJsonData[i].keys()
-    key = next(iter(keys))
-    listEntites.append(key)
-    #print(key)
+#myJsonData = extractionJSON.parseJSON(XJ_Convertor.fichierInput)
+# Recuperons la liste des entites, en appelant la fonction getEntites() dans extractionJSON
+listEntites = extractionJSON.getEntites(myJsonData)
 
-# Initiation du fichier SVG
-svg_document = svgwrite.Drawing(filename = 'json_svg.svg',
+# Creation du contexte du fichier SVG
+svg_document = svgwrite.Drawing(filename = XJ_Convertor.fichierOutput,
                                 debug = True)
-entityCoordsList = []
-# Recuperation des noms des entités que l'on enregistre dans la variable "listEntites"
+coordsEntiteList = []   # les coordonnees des entites
+
 for i in range(len(listEntites)):
     listAttributs = []
     listAssocs = []
-    # Recuperation des noms des attributs que l'on enregistre dans la variable "listAttributs"
+    # Recuperons les noms des attributs et stockons les dans listAttributs
     nbAttributs = len(myJsonData[i][listEntites[i]][0].keys())
     
     for key in myJsonData[i][listEntites[i]][0].keys():
         listAttributs.append(key)
         #print('\t\t' + key)
 
-    if (i % 2 == 0):
-        # Creation du rectangle contenant les infos de l'entité
-        svg_document.add(svg_document.rect(insert = (10, i*150 + 10),
-                                           size = ("150px", "130px"),
+    if (i % 2 == 0 ): # Pour les entites sur la premiere colonne
+        # Rectangle contenant l'ensemble des attributs de chacun des entites
+        svg_document.add(svg_document.rect(insert = (40, i*(i+150) + 10),
+                                           size = ("150px", "150px"),
                                            stroke_width = "1",
                                            stroke = "black",
-                                           fill = "rgb(209, 250, 46)"))
+                                           fill = "#B0C4DE"))
         entityCoords = {
             "nomEntite": listEntites[i],
             "coordX": 10,
-            "coordY": i*150 + 10
+            "coordY": i*(i+150) + 10
         }
-        entityCoordsList.append(entityCoords)          
-
-        svg_document.add(svg_document.rect(insert = (10, i*150 + 10),
+        coordsEntiteList.append(entityCoords)          
+        # Rectangle contenant le nom de l'entite
+        svg_document.add(svg_document.rect(insert = (40, i*(i+150) + 10),
                                            size = ("150px", "26px"),
                                            stroke_width = "1",
                                            stroke = "black",
-                                           fill = "rgb(209, 250, 46)"))
+                                           fill = "#B0C4DE"))
         
 
         # Affichage du nom de l'entité
         svg_document.add(svg_document.text(listEntites[i],
-            insert=(20, i*150 + 30),
+            insert=(44, i*(i+150) + 30),
             stroke='none',
             fill="rgb(42, 42, 42)",
             font_size='15px',
             font_weight="bold")
         )
 
-        # Affichage de la liste des attributs
+        # Affichons l'ensemble des attributs de chacun des entites
         for k in range(len(listAttributs)):
             svg_document.add(svg_document.text(listAttributs[k],
-                insert=(20, i*150 + 40 + (k+1)*20),
+                insert=(44, i*(i+150) + 40 + (k+1)*20),
                 stroke='none',
-                fill="rgb(15, 15, 15)",
+                fill="rgb(15, 15, 15)", #couleur de
                 font_size='15px',
                 font_weight="bold")
             )
-    else:
-        # Creation du rectangle contenant les infos de l'entité
+    
+    else: #Pour les entites sur la deuxieme colonne
+        # Rectangle contenant l'ensemble des attributs de chacun des entites
         svg_document.add(svg_document.rect(insert = (400, (i - 1)*150 + 10),
-                                           size = ("150px", "130px"),
+                                           size = ("150px", "150px"),
                                            stroke_width = "1",
                                            stroke = "black",
-                                           fill = "rgb(209, 250, 46)"))
+                                           fill = "#B0C4DE"))
 
         entityCoords = {
             "nomEntite": listEntites[i],
             "coordX": 400,
             "coordY": (i - 1)*150 + 10
         }
-        entityCoordsList.append(entityCoords)
+        coordsEntiteList.append(entityCoords)
 
+        #Rectangle contenant le nom de l'entite
         svg_document.add(svg_document.rect(insert = (400, (i - 1)*150 + 10),
                                            size = ("150px", "26px"),
                                            stroke_width = "1",
                                            stroke = "black",
-                                           fill = "rgb(209, 250, 46)"))
-
-        
+                                           fill = "#B0C4DE"))
 
         # Affichage du nom de l'entité
         svg_document.add(svg_document.text(listEntites[i],
@@ -99,7 +100,7 @@ for i in range(len(listEntites)):
             font_weight="bold")
         )
 
-        # Affichage de la liste des attributs
+        # Affichons l'ensemble des attributs de chacun des entites
         for k in range(len(listAttributs)):
             svg_document.add(svg_document.text(listAttributs[k],
                 insert=(410, (i - 1)*150 + 40 + (k+1)*20),
@@ -109,3 +110,6 @@ for i in range(len(listEntites)):
                 font_weight="bold")
             )
 svg_document.save()
+print("-------------------------------------------------------------------------")
+print("le fichier ",XJ_Convertor.fichierOutput," a ete bien generer" )
+print("-------------------------------------------------------------------------")
